3876bcda971880c602e679d19ecd6032b8fd02d0
shishir <shishirlamichhane@gmail.com>
2014-11-07 22:45:01 +0545

    Checked in notifier

--- app/actors/Notifier.scala
+++ app/actors/Notifier.scala
@@ -7,65 +7,48 @@ import play.api.libs.json.Json
 /**
  * Created by shishir on 8/20/14.
  */
-case class Member(id: String, val receiver: ActorRef, val sender: ActorRef)
-
 object Notifier {
 
-  case class DisconnectMember(ref: ActorRef)
+  case class Connected(id: String, ref: ActorRef)
+  case class Exited(id: String, ref: ActorRef)
 
-  var members = Map.empty[String, Member]
   def props(out: ActorRef, sup: ActorRef, id: String) = Props(new Notifier(out, sup, id))
 
 }
-
-//TODO: http://stackoverflow.com/questions/26476827/private-communication-between-client-and-server-using-websockets-play-2-3-x
 
 class Notifier(out: ActorRef, sup: ActorRef, id: String) extends Actor with ActorLogging {
 
   /*val myActor = context.actorOf(MyActor.props(self), "member-"+id)*/
 
-
-  /*val c = (sendActor ? Init(id, receiveActor)).map{
-    case c: ConnectedWS[_] =>
-      play.Logger.info(s"Connected Member with ID:$id")
-      members = members + (id -> Member(id, receiveActor, sendActor))
-      c
-  }*/
-
   import Notifier._
   import Supervisor._
 
   def receive = {
-    /*case no: Connect => {
-      if (!members.contains(id)) {
-        log.info(id)
-        members = members + (id -> Member(id, out, sender))
-      }
-      val account: WsResponse = new WsResponse("Connect", "note", Json.toJson("connected"))
-      out ! account
-    }*/
+
+    case Connected(id, ref) => {
+      val connectedMsg: WsResponse = new WsResponse("Connected", id, Json.toJson("connected"))
+      ref ! connectedMsg
+    }
+
+    case Exited(id, ref) => {
+      val exitMsg: WsResponse = new WsResponse("Disconnected", id, Json.toJson("exited"))
+      ref ! exitMsg
+    }
+
     case di: SendMessage => {
       log.info("Reached SendMessage case in Notifier")
       val account: WsResponse = new WsResponse("Message", id, Json.toJson(di.message))
       sup ! SuperSend(di.to, account)
     }
 
-    case ro: Disconnect => {
-      val account: WsResponse = new WsResponse("activity", "rough", Json.obj("msg" -> "disconnected"))
-      members.get(id).foreach{ m =>
-        members = members - id
-        m.receiver ! PoisonPill
-        m.sender ! PoisonPill
-      }
-    }
     case lm: ListMemberIds => {
-      val account: WsResponse = new WsResponse("activity", "rough", Json.obj("msg" -> members.map(_._1).toSeq))
-      membersMap.get(id).get ! account
+      val account: WsResponse = new WsResponse("List", "rew3", Json.obj("msg" -> membersMap.map(_._1).toSeq))
+      out ! account
     }
   }
 
   override def preStart(): Unit = {
-    sup ! Connected(id, out)
+    sup ! InitConnection(id, out)
   }
 
   override def postStop(): Unit = {
--- app/actors/Supervisor.scala
+++ app/actors/Supervisor.scala
@@ -1,5 +1,6 @@
 package actors
 
+import actors.Notifier.Exited
 import akka.actor._
 import models.WsResponse
 import play.api.libs.json.{JsObject, Json}
@@ -12,7 +13,7 @@ import play.api.libs.json.{JsObject, Json}
 object Supervisor {
 
   case class SuperSend(id: String, msg: WsResponse)
-  case class Connected(id: String, ref: ActorRef)
+  case class InitConnection(id: String, ref: ActorRef)
   case class Disconnect(id: String)
 
   var membersMap = Map[String, ActorRef]()
@@ -21,38 +22,33 @@ object Supervisor {
 
 class Supervisor extends Actor with ActorLogging {
 
-  import Notifier._
+  import Notifier.Connected
   import Supervisor._
 
   def receive = {
-    /*case Disconnect => {
-      superVisor ! DisconnectMember(self)
-      }*/
 
-    case Connected(id, out) => {
+    case InitConnection(id, out) => {
+      context.watch(out)
       if (!membersMap.contains(id)) {
+        membersMap.map(m => sender ! Connected(id, m._2))
         membersMap += id -> out
-        log.info(s"Connected id: $id")
       }
     }
 
     case SuperSend(to, msg) => {
-      log.info("Reached SuperSend in Supervisor")
       membersMap.get(to).get ! msg
-      if(membersMap.exists(_._1 == to)) {
-        log.info(s"Member obtained ${membersMap.get(to).get}")
-        val sender = context.actorOf(Sender.props(self), "member-" + to)
-        sender ! msg
-      }
     }
 
     case Disconnect(id) => {
-      log.info("disconnecting member")
+      log.info(s"disconnecting member $id")
+      val exitMsg: WsResponse = new WsResponse("Disconnected", id, Json.toJson("exited"))
       membersMap.get(id).foreach{ m =>
-        m ! PoisonPill
         membersMap -= id
       }
+      membersMap.map(m => m._2 ! exitMsg)
     }
+
+    case Terminated(out) => log.info(s"The Actor Path ${out.path} terminated")
 
     }
 }
